b184b0362519b47a5b44d0e5e674b4b1b1d8d453
Devendra Kumar <80640817+Davin-X@users.noreply.github.com>
2021-12-18 11:41:52 +0530

    upadated

--- interview_query_5.sql
+++ interview_query_5.sql
@@ -1,10 +1,5 @@
 
 /* nterview questions
-
-
-*/
-
-CREATE TABLE tmp (id int );
 /*
 1Q) input data
   ID
@@ -21,8 +16,13 @@ Output
  4,5
  */
 
+*/
 
-Select id as A ,lead(id) over (ORDER BY id) as B FROM tmp;
+CREATE TABLE table_1 (id int );
+INSERT INTO table_1 values (1),(2),(3),(4),(5);
+
+
+Select id as A ,lead(id) over (ORDER BY id) as B FROM table_1;
 
 /* 
 2Q) calculate positive numbers and negative numbers.
@@ -40,9 +40,12 @@ Output
 --------------
 6,-6
 */
-select sum(case when id >=0 then id else 0 end) as positive,
-sum(case when id  <0 then id else 0 end) as negative
-from tmp;
+
+CREATE TABLE table_2 (nums int );
+INSERT INTO table_2 values (1),(2),(3),(-3),(-2),(-1);
+select sum(case when nums >=0 then nums else 0 end) as positive,
+sum(case when nums  <0 then nums else 0 end) as negative
+from table_2;
 
 /*
 
